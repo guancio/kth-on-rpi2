f4edde587c4852e20fa91f36deebb58f4b5b7f18
Roberto Guanciale <robertog@csc.kth.se>
2015-06-16 12:15:58 +0200

    chenged the deploy script

--- test/linux/deploy.sh
+++ test/linux/deploy.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-sudo mount -o loop,offset=$[106496*512] ../../../sth_deps/beagle_sd_working.img ../../../mnt/
-sudo cp ../../core/build/sth_beagleboard.fw.img ../../../mnt/boot/uImage
-#sudo cp ../sth_sics/sth_dynamic/core/build/sth_beagleboard.fw.img ../mnt/boot/uImage
-sudo umount ../../../mnt/
+IMG_NAME=beagle_sd_working.img
+IMG_DIR=../../../sth_deps
+IMG_FILE=$IMG_DIR/$IMG_NAME
+MNT_POINT=../../../mnt/
+
+if [ -f $IMG_FILE ];
+then
+   echo ""
+else
+   echo "copying the default image"
+   cp $IMG_DIR/beagle_sd.img $IMG_FILE
+fi
+
+if [ -d $MNT_POINT ];
+then
+   echo ""
+else
+   echo "creating the mount point"
+   mkdir $MNT_POINT
+fi
+
+echo "creating the SD image"
+sudo mount -o loop,offset=$[106496*512] $IMG_FILE $MNT_POINT
+sudo cp ../../core/build/sth_beagleboard.fw.img $MNT_POINT/boot/uImage
+sudo umount $MNT_POINT
