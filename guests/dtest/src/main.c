3f0539ac8e1cf648bcea5114430e268f2e9909b0
Roberto Guanciale <robertog@csc.kth.se>
2015-07-30 18:20:24 +0200

    Fixed the TEST_DMMU_L2_UNMAP_PT test

--- guests/dtest/src/main.c
+++ guests/dtest/src/main.c
@@ -505,9 +505,9 @@ void test_l2_unmap_pt()
 
 
 	// Creating an L2 to map
-	va = (va_base | (uint32_t)0x100000);
+	va = (va_base | (uint32_t)0x300000);
 	pa = va2pa(va);
-	pga = va2pa(va_base |(uint32_t)0x300000);
+	pga = va2pa(va_base |(uint32_t)0x400000);
 
 	ISSUE_DMMU_HYPERCALL(CMD_UNMAP_L1_PT_ENTRY, va, 0, 0);
 	res= ISSUE_DMMU_HYPERCALL(CMD_CREATE_L2_PT, pa, 0, 0);
